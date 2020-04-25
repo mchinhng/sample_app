79919dac1affb46de2d808ac789a97ba0e35d064
Minh Chinh <nguyentmchinh.pd@gmail.com>
2020-04-25 23:34:09 +0700

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ ruby '2.6.6'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.2'
 # Use sqlite3 as the database for Active Record
 gem 'bcrypt', '3.1.13' 
+gem 'faker', '2.1.2' 
+gem 'will_paginate', '3.1.8' 
+gem 'bootstrap-will_paginate', '1.0.0' 
 gem 'bootstrap-sass', '3.4.1' 
 gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-  def show 
-  	@user = User.find(params[:id]) 
-  end 
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy] 
+  before_action :correct_user, only: [:edit, :update]  
+  before_action :admin_user, only: :destroy 
+
+  @user = User.find(params[:id]) 
+
+def index 
+  @users = User.paginate(page: params[:page])  
+end 
 
   def new 
   	@user = User.new 
@@ -17,10 +23,48 @@ class UsersController < ApplicationController
   	render 'new' 
   end 
   end
-  
+
+  def edit 
+    @user = User.find(params[:id]) 
+  end 
+
+  def update 
+    @user = User.find(params[:id]) 
+    if @user.update(user_params) 
+    flash[:success] = "Profile updated" 
+    redirect_to @user 
+    else 
+    render 'edit' 
+    end 
+  end 
+
+  def destroy 
+    User.find(params[:id]).destroy 
+    flash[:success] = "User deleted" 
+    redirect_to users_url 
+  end 
+
   private 
   	def user_params 
   		params.require(:user).permit(:name, :email, :password, 
   								:password_confirmation) 
   	end  
+  # Before filters 
+  # Confirms a logged-in user. 
+    def logged_in_user 
+      unless logged_in? 
+        store_location 
+        flash[:danger] = "Please log in." 
+        redirect_to login_url 
+      end 
+    end
+
+    def correct_user 
+     @user = User.find(params[:id]) 
+      redirect_to(root_url) unless current_user?(@user)  
+    end
+    
+    def admin_user 
+      redirect_to(root_url) unless current_user.admin? 
+    end
 end
